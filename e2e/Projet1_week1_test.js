23bff3e965e8c58235aa0d7c21378a311318c025
ptitRastignac <55018150+petitRastignac@users.noreply.github.com>
2020-09-25 15:25:30 +0200

    Update Projet1_week1_test.js

--- e2e/Projet1_week1_test.js
+++ e2e/Projet1_week1_test.js
@@ -2,11 +2,11 @@ Feature('Projet1_week1');
 
 let root = "http://localhost:8080/Projet_1/";
 
-let usr = "tester_codecept";
-let pass = "ImWatchingU";
+let usr = "laure";
+let pass = "dinateur";
 
-let usr_reg = usr;
-let pass_reg = pass;
+let usr_reg = "laure";
+let pass_reg = "dinateur";
 
 
 Scenario('Test Registration', (I) => {
@@ -41,7 +41,7 @@ Scenario('Test Login', (I) => {
     I.wait(2);
 });
 
-Scenario('Test Browse without being logged', (I) => {
+Scenario('Test Browse being logged', (I) => {
     I.amOnPage(root);
 
     I.fillField("uname", usr);
@@ -51,9 +51,21 @@ Scenario('Test Browse without being logged', (I) => {
     I.click("Acceuil");
     I.see("Acceuil");
     I.click("Login");
-    I.see("Acceuil");
+    I.see("Logout");
     I.click("Questions");
     I.see("Question");
     I.click("Profil");
     I.see("John Doe");
+});
+
+Scenario('Test login logout', (I) => {
+    I.amOnPage(root);
+
+    I.fillField("uname", usr);
+    I.fillField("psw", pass);
+    I.pressKey('Enter');
+    I.see("Acceuil");
+    I.click("Login");
+    I.click("logout");
+    I.see("Username");
 });

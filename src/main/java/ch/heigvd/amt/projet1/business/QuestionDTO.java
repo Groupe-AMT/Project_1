6d0bef5c6be1e5e491380cd0aef1e957acf4c033
csemblat <csemblat@protonmail.com>
2020-09-29 23:06:18 +0200

    changed IQuestionRepository

--- src/main/java/ch/heigvd/amt/projet1/business/QuestionDTO.java
+++ src/main/java/ch/heigvd/amt/projet1/business/QuestionDTO.java
@@ -1,4 +1,17 @@
 package ch.heigvd.amt.projet1.business;
 
+import java.util.List;
+
 public class QuestionDTO {
+    private String author;
+    private String content;
+    private String subject;
+    private List<String> tags;
+
+    public QuestionDTO(String author, String content, String subject, List<String> tags){
+        this.author = author;
+        this.content = content;
+        this.subject = subject;
+        this.tags = tags;
+    }
 }
--- src/main/java/ch/heigvd/amt/projet1/model/IQuestionRepository.java
+++ src/main/java/ch/heigvd/amt/projet1/model/IQuestionRepository.java
@@ -3,5 +3,6 @@ package ch.heigvd.amt.projet1.model;
 import java.util.Collection;
 
 public interface IQuestionRepository {
-    public Collection<Question> find(QuestionQuery query);
+    public Collection<Question> findByTag(String tag);
+    public Collection<Question> findByAuthor(String author);
 }

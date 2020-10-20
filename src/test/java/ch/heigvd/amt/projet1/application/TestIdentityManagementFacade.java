51d748949660aa0b34d9c1ad035e92f361ffe215
jerome <jerome.arn@heig-vd.ch>
2020-10-20 22:14:58 +0200

    test register OK

--- src/test/java/ch/heigvd/amt/projet1/application/TestIdentityManagementFacade.java
+++ src/test/java/ch/heigvd/amt/projet1/application/TestIdentityManagementFacade.java
@@ -3,6 +3,7 @@ import ch.heigvd.amt.projet1.application.identitymanagement.login.RegisterComman
 import ch.heigvd.amt.projet1.application.identitymanagement.login.RegisterFailedException;
 import ch.heigvd.amt.projet1.domain.person.IPersonRepository;
 import ch.heigvd.amt.projet1.domain.person.Person;
+import ch.heigvd.amt.projet1.domain.person.PersonId;
 import org.junit.jupiter.api.Test;
 import ch.heigvd.amt.projet1.application.identitymanagement.IdentityManagementFacade;
 
@@ -15,14 +16,29 @@ import static org.junit.jupiter.api.Assertions.*;
 public class TestIdentityManagementFacade {
     IPersonRepository IPR = mock(IPersonRepository.class);
     IdentityManagementFacade id = new IdentityManagementFacade(IPR);
-    RegisterCommand Rc = new RegisterCommand("jerome_A","jerome@heig-vd.ch","A", "jerome","password");
-    Optional<Person> P = Optional.empty();
+    RegisterCommand Rc = RegisterCommand.builder()
+            .username("jerome_A")
+            .firstname("jerome@heig-vd.ch")
+            .lastname("A")
+            .email("jerome@heig-vd.ch")
+            .clearPassword("password")
+            .build();
+    Person p = new Person(new PersonId(),"jerome_A","jerome@heig-vd.ch","jerome", "A","password");
 
     @Test
-    void Testregister() {
+    void TestregisterUserDontExist() {
+        Optional<Person> P = Optional.of(p);
         when(IPR.findByUsername(anyObject())).thenReturn(P);
         when(IPR.save(anyObject())).thenReturn(1);
         assertThrows(RegisterFailedException.class, ()->id.register(Rc));
+    }
+
+    @Test
+    void TestregisterUserAlreadyExist() {
+        Optional<Person> P = Optional.empty();
+        when(IPR.findByUsername(anyObject())).thenReturn(P);
+        when(IPR.save(anyObject())).thenReturn(1);
+        assertDoesNotThrow(()->id.register(Rc));
     }
 
     @Test

fe8e42ef308c1e480b7eba2efc1779802faf8781
jerome <jerome.arn@heig-vd.ch>
2020-10-21 20:44:40 +0200

    modif script arquillian

--- arqui.sh
+++ arqui.sh
@@ -2,7 +2,9 @@
 mvn liberty:stop
 
 mvn clean package
-mvn liberty:create liberty:install-feature liberty:deploy
+mvn liberty:create
+mvn liberty:install-feature
+mvn liberty:deploy
 mvn liberty:start
 #mvn liberty:configure-arquillian
 

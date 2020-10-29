90a689c93c5ec85f837cbbbf0b92c0b7da704b3d
jerome <jerome.arn@heig-vd.ch>
2020-10-29 19:29:05 +0100

    fin test unitaires

--- src/test/java/ch/heigvd/amt/projet1/application/statisticmanagement/TestStatisticManagement.java
+++ src/test/java/ch/heigvd/amt/projet1/application/statisticmanagement/TestStatisticManagement.java
@@ -6,9 +6,10 @@ import ch.heigvd.amt.projet1.domain.answer.IAnswerRepository;
 import ch.heigvd.amt.projet1.domain.comment.ICommentRepository;
 import ch.heigvd.amt.projet1.domain.person.IPersonRepository;
 import ch.heigvd.amt.projet1.domain.question.IQuestionRepository;
+import ch.heigvd.amt.projet1.domain.statistic.Statistic;
 import org.junit.jupiter.api.Test;
-import java.util.List;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Matchers.anyString;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
@@ -17,14 +18,34 @@ public class TestStatisticManagement {
     IAnswerRepository IAR = mock(IAnswerRepository.class);
     ICommentRepository ICR = mock(ICommentRepository.class);
     IPersonRepository IPR = mock(IPersonRepository.class);
+    StatisticManagementFacade STMF = new StatisticManagementFacade(IQR,IAR,ICR,IPR);
 
     @Test
     void testGetStats(){
+        when(IQR.Size()).thenReturn(1);
+        when(IAR.Size()).thenReturn(2);
+        when(ICR.Size()).thenReturn(3);
+        when(IPR.Size()).thenReturn(4);
+        when(IQR.SizeFor(anyString())).thenReturn(5);
+        when(IAR.SizeFor(anyString())).thenReturn(6);
+        when(ICR.SizeFor(anyString())).thenReturn(7);
 
+        Statistic stat = Statistic.builder()
+                .nbQuestions(1)
+                .nbAnswers(2)
+                .nbComments(3)
+                .nbUsers(4)
+                .nbSelfQuestions(5)
+                .nbSelfAnswers(6)
+                .nbSelfComments(7)
+                .build();
+
+        assertEquals(stat, STMF.getStats("username"));
     }
 
     @Test
     void testQuestionSize(){
-
+        when(IQR.Size()).thenReturn(20);
+        assertEquals(20, STMF.getQuestionSize());
     }
 }

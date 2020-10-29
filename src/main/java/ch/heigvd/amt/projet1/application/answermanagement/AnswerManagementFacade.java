8096b947ae09eb40f4e8475f1fda14caf807451a
jerome <jerome.arn@heig-vd.ch>
2020-10-29 20:19:51 +0100

    refactor

--- src/main/java/ch/heigvd/amt/projet1/application/answermanagement/AnswerManagementFacade.java
+++ src/main/java/ch/heigvd/amt/projet1/application/answermanagement/AnswerManagementFacade.java
@@ -3,7 +3,6 @@ package ch.heigvd.amt.projet1.application.answermanagement;
 import ch.heigvd.amt.projet1.domain.answer.Answer;
 import ch.heigvd.amt.projet1.domain.answer.IAnswerRepository;
 import ch.heigvd.amt.projet1.domain.question.QuestionId;
-
 import java.util.List;
 
 public class AnswerManagementFacade {
@@ -15,20 +14,17 @@ public class AnswerManagementFacade {
 
     public double saveAnswer(AnswerCommand command) throws AnswerException {
         try {
-            Answer newAnswer = Answer.builder()
+            answerRepository.save(Answer.builder()
                     .author(command.getAuthor())
                     .questionId(command.getQuestionId())
                     .content(command.getContent().replaceAll("'"," "))
-                    .build();
-
-            answerRepository.save(newAnswer);
+                    .build());
         }catch (Exception e){
             throw new AnswerException(e.getMessage());
         }
         return 1;
     }
     public List<Answer> getRelatedAnswer(QuestionId id){
-        List<Answer> relatedAnswer = (List<Answer>) answerRepository.findAllforQuestion(id);
-        return relatedAnswer;
+        return answerRepository.findAllforQuestion(id);
     }
 }
